6727e240d99e90df958d073a11529d703113e9b5
Mauricio Martinez <mauricio.martinez@premise.com>
2023-08-30 22:10:09 -0600

    Update Autocomplete component for DynamicPickExtension

--- packages/dynamic-pick-extension/src/components/DynamicPickExtension.tsx
+++ packages/dynamic-pick-extension/src/components/DynamicPickExtension.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { InputLabel, FormControl, Select, MenuItem, LinearProgress, FormHelperText } from '@material-ui/core';
+import React, { useState } from 'react';
+import { TextField } from '@material-ui/core';
 import { z } from 'zod';
 import { makeFieldSchemaFromZod } from '@backstage/plugin-scaffolder';
 import { useAsync } from 'react-use';
-import { useApi, configApiRef } from '@backstage/core-plugin-api'
+import { useApi, configApiRef } from '@backstage/core-plugin-api';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const DynamicPickExtensionWithOptionsFieldSchema = makeFieldSchemaFromZod(
   z.string(),
@@ -32,39 +33,38 @@ export const DynamicPickExtension = ({
 }: DynamicPickExtensionWithOptionsProps) => {
   const config = useApi(configApiRef);
   const backendUrl = config.getString('backend.baseUrl');
-  let formDataOptions: any = []
-  if (options?.external_data) {
-    formDataOptions = useAsync(async () => {
-      return (await fetch(options?.external_data)).json();
-    })
-  } else if (options?.form_data) {
-    formDataOptions = useAsync(async () => {
-      return (await fetch(`${backendUrl}/api/form-data/${options?.form_data}`)).json();
-    })
-  }
-  
+  const [loading, setLoading] = useState(true);
+  const [formDataOptions, setFormDataOptions] = useState([]);
+
+  useAsync(async () => {
+    if (options?.external_data) {
+      setFormDataOptions(await (await fetch(options?.external_data)).json());
+      setLoading(false);
+    } else if (options?.form_data) {
+      setFormDataOptions(await (await fetch(`${backendUrl}/api/form-data/${options?.form_data}`)).json());
+      setLoading(false);
+    }
+  });
+
   return (
-    <FormControl
-      margin="normal"
-      required={required}
-      error={rawErrors?.length > 0 && !formData}
-    >
-      <InputLabel >{title}</InputLabel >
-      <Select
-        label={title}
-        id={idSchema?.$id}
-        value={formData ?? ""}
-        onChange={({ target: { value } }) => onChange(value)}
-        >
-        {(()=> {
-          try {
-            return formDataOptions.value?.map((option: any) => <MenuItem key={option} value={option}>{option}</MenuItem>)
-          } catch (error) {
-            return <MenuItem value="Error">Error</MenuItem>
-          }
-        })()}
-        {formDataOptions.loading && <LinearProgress />}
-      </Select>
-      {description && <FormHelperText>{description}</FormHelperText>}
-    </FormControl >)
+    <Autocomplete
+      id={idSchema?.$id}
+      loading={loading}
+      value={formData ?? null}
+      renderInput={(params) =>
+        <TextField
+          {...params}
+          label={title}
+          variant="standard"
+          required={required}
+          error={rawErrors?.length > 0 && !formData}
+          helperText={description}
+        />
+      }
+      options={formDataOptions}
+      onChange={(_, value) => onChange(value)}
+      getOptionSelected={(option, value) => option === value}
+      disableClearable
+    />
+  )
 };
